b154821e896a8e16ce695240473bac134203bdc8
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-05-17 08:48:51 +0200

    added supervisor process install

--- stage2/03-automatica/02-run-chroot.sh
+++ stage2/03-automatica/02-run-chroot.sh
@@ -1,1 +1,8 @@
 ldconfig -v
+
+curl -sSL https://get.docker.com | sh
+systemctl enable docker
+
+docker pull automaticacore/automatica-supervisor:develop-latest
+
+docker run --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock automaticacore/automatica-supervisor:develop-latest
