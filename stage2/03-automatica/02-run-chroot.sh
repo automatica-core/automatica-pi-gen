14811a8f73b1b9d9bed6cb740b6753b157d36660
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-05-20 22:13:05 +0200

    updated runchroot script

--- stage2/03-automatica/02-run-chroot.sh
+++ stage2/03-automatica/02-run-chroot.sh
@@ -1,8 +1,17 @@
 ldconfig -v
 
-curl -sSL https://get.docker.com | sh
-systemctl enable docker
+cat /proc/cpuinfo
 
+DOCKER_RET=$(docker -v)
+
+if [ $DOCKER_RET -ne 0 ]; then
+    curl -sSL https://get.docker.com | sh
+    systemctl enable docker
+fi
+echo "starting docker..."
+dockerd &
+
+usermod -aG docker automatica
 docker pull automaticacore/automatica-supervisor:develop-latest
 
 docker run --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock automaticacore/automatica-supervisor:develop-latest
