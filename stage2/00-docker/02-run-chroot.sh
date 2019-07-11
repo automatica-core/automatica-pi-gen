02b071d39e0aa782e6ce4a5e933fd84bde6fd489
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-07-11 14:12:05 +0200

    fixed build

--- stage2/00-docker/02-run-chroot.sh
+++ stage2/00-docker/02-run-chroot.sh
@@ -1,11 +1,11 @@
 # Get the Docker signing key for packages
 echo "installing docker deb key"
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+curl --insecure https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
 
 echo "adding docker deb to packages"
 # Add the Docker official repos
-echo "deb [arch=armhf] https://download.docker.com/linux/stretch \
-     $(lsb_release -cs) stable" | \
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     stretch stable" | \
     tee /etc/apt/sources.list.d/docker.list
 
 apt-get update
