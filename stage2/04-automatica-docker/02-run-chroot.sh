a49ada35cece5a8a66bb8518523901229eada265
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2019-07-14 18:33:33 +0200

    remove file if existing

--- stage2/04-automatica-docker/02-run-chroot.sh
+++ stage2/04-automatica-docker/02-run-chroot.sh
@@ -8,6 +8,10 @@ then
     exit 0
 fi
 
+rm -f /etc/systemd/system/multi-user.target.wants/docker.service
+rm -f /etc/systemd/system/multi-user.target.wants/mariadb.service
+rm -f /etc/systemd/system/multi-user.target.wants/supervisor.service
+
 ln -s /lib/systemd/system/docker.service /etc/systemd/system/multi-user.target.wants/docker.service
 ln -s /lib/systemd/system/mariadb.service /etc/systemd/system/multi-user.target.wants/mariadb.service
 ln -s /lib/systemd/system/supervisor.service /etc/systemd/system/multi-user.target.wants/supervisor.service
