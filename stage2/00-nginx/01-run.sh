565fede965c24c073cd4a77db90e5c294e5f682b
Patrik Pfaffenbauer <patrik.pfaffenbauer@p3-software.eu>
2020-04-14 18:29:05 +0200

    update nginx to https and add certificate generation

--- stage2/00-nginx/01-run.sh
+++ stage2/00-nginx/01-run.sh
@@ -3,9 +3,18 @@
 rm -f "${ROOTFS_DIR}/etc/nginx/sites-enabled/default"
 
 install -v -m 644 files/nginx-automatica-config		"${ROOTFS_DIR}/etc/nginx/sites-available/automatica-app"
+install -v -m 644 files/proxy.conf		"${ROOTFS_DIR}/etc/nginx/proxy.conf"
+
 rm -f "${ROOTFS_DIR}/etc/nginx/sites-enabled/automatica-app"
 
 pwd=$(pwd)
 cd "${ROOTFS_DIR}/etc/nginx/sites-enabled"
 ln -s "../sites-available/automatica-app" .
 cd $pwd
+
+
+echo create certificate
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout files/automatica.key -out files/automatica.crt -config files/localhost.conf
+install -v -m 644 files/automatica.crt "${ROOTFS_DIR}/etc/ssl/certs/automatica.crt"
+install -v -m 644 files/automatica.key "${ROOTFS_DIR}/etc/ssl/certs/automatica.key"
