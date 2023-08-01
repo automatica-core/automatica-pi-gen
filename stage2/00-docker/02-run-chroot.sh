5e6df9436aecf88a0f4bdd963b414892b26c390c
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-01 17:28:24 +0200

    update build

--- stage2/00-docker/02-run-chroot.sh
+++ stage2/00-docker/02-run-chroot.sh
@@ -19,8 +19,8 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=armhf signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  bullseye stable | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
