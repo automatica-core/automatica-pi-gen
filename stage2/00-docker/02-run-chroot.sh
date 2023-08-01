22bff6d8b76f0bf6e7713292dd7a22f8137e651e
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-01 17:26:47 +0200

    update build

--- stage2/00-docker/02-run-chroot.sh
+++ stage2/00-docker/02-run-chroot.sh
@@ -11,17 +11,21 @@ then
 fi
 
 # Get the Docker signing key for packages
-echo "installing docker deb key"
-curl --insecure https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
+apt-get update
+apt-get install ca-certificates curl gnupg
 
-echo "adding docker deb to packages"
-# Add the Docker official repos
-echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-     stretch stable" | \
-    tee /etc/apt/sources.list.d/docker.list
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 
 apt-get update
 
-apt-get -y install docker-ce --no-install-recommends
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 usermod -aG docker automatica
--- stage2/04-automatica-docker/02-run-chroot.sh
+++ stage2/04-automatica-docker/02-run-chroot.sh
@@ -18,3 +18,5 @@ fi
 ln -s /lib/systemd/system/docker.service /etc/systemd/system/multi-user.target.wants/docker.service
 ln -s /lib/systemd/system/mariadb.service /etc/systemd/system/multi-user.target.wants/mariadb.service
 ln -s /lib/systemd/system/supervisor.service /etc/systemd/system/multi-user.target.wants/supervisor.service
+
+rm -f /etc/systemd/system/multi-user.target.wants/userconfig.service
