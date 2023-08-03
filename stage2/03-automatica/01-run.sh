e537af68f560f08e0bc4308f91ee8bfd72e547ca
Patrik Pfaffenbauer <patrik.pfaffenbauer@live.at>
2023-08-03 15:34:52 +0200

    fix standalone setup

--- stage2/03-automatica/01-run.sh
+++ stage2/03-automatica/01-run.sh
@@ -7,22 +7,18 @@ echo "Install native = $INSTALL_NATIVE"
 if [ "$INSTALL_NATIVE" != "1" ];
 then
     echo "ignore installing automatica native"
-    rm -f	"${ROOTFS_DIR}/etc/systemd/system/automatica.core.service.d"
     exit 0
 fi
 
-install -v -d	"${ROOTFS_DIR}/etc/systemd/system/automatica.core.service.d"
-install -v -m 644 files/service-automatica-config "${ROOTFS_DIR}/etc/systemd/system/automatica.core.service.d/automatica.core.service"
-dos2unix ${ROOTFS_DIR}/etc/systemd/system/automatica.core.service.d/automatica.core.service
+install -v -m 644 files/service-automatica-config "${ROOTFS_DIR}/lib/systemd/automatica/automatica.service"
+dos2unix "${ROOTFS_DIR}/lib/systemd/automatica/automatica.service"
 
 install -v -m 644 files/10-usb.rules "${ROOTFS_DIR}/etc/udev/rules.d/10-usb.rules"
 
 pwd=$(pwd)
 cd ${ROOTFS_DIR}/etc/systemd/system/
-rm -f automatica.core.service
-rm -f ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/automatica.core.service
-ln -s automatica.core.service.d/automatica.core.service automatica.core.service
-ln -s automatica.core.service.d/automatica.core.service ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/automatica.core.service
+rm -f ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/automatica.service
+ln -s /lib/systemd/automatica/automatica.service ${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/automatica.service
 
 cd $pwd
 
